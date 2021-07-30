f0425720d866e734d340a7b3d9c19b6bff7852e0
sachanritik1 <sachanritik1@gmail.com>
2021-07-30 15:00:11 +0530

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import RestaurantsDAO from "../dao/restaurantsDAO.js";
+
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
+
+const port = process.env.PORT || 8000;
+
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+})
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await RestaurantsDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`server is listening to the port ${port}`);
+    });
+  });
